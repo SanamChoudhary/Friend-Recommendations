b1741316fd504fc6ea9bdc8bb9f38002940c2192
SanamChoudhary <sanamc541@gmail.com>
2025-04-02 23:57:33 -0500

    Completed the Logic for finding friend suggestions

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from network import network
 
 def suggestFriends(name):
-    allPeople = []
-    for person in network:
-        if person not in allPeople:
-            allPeople.append(person)
+   
+    #Goes to network dict and returns the list of friends that "name" has
+    relationship = network[name]
 
-    currentPerson = ""
-    for person in allPeople:
-        if name == person:
-            currentPerson = person
-            break
+    #list of recommended friends
+    recommendedFriends = []
 
-
-
-
-
+    #Iterates through every name the relationships that "Name" has
+    for relation in relationship:
+        nameRelationships = network[relation]
+        nameRelationships.remove(name)
+        for friend in nameRelationships:
+            recommendedFriends.append(friend)
+    
+    recommendedFriends = set(recommendedFriends)
+    recommendedFriends = list(recommendedFriends)
+    
+    print("Suggested friends for " + name + ":")
+    for person in recommendedFriends:
+        print(person)
+        
 name = input("Enter a name to suggest friends for: ")
 suggestFriends(name)
 
--- network.py
+++ network.py
@@ -2,6 +2,6 @@ network = {
     "Alice": ["Bob", "Carol"],
     "Bob": ["Alice", "Dave"],
     "Carol": ["Alice", "Eve"],
-    "Dave": ["Bob"],
-    "Eve": ["Carol"]
+    "Dave": ["Bob", "Eve"],
+    "Eve": ["Carol","Dave"]
 }
